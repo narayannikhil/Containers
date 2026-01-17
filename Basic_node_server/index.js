e6d76678b8664b8392245b8c8c6f033403236fb6
naryannikhil <nikhilpareeknp@gmail.com>
2026-01-17 09:40:44 +0530

    basic_node_server

---
+++ Basic_node_server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/home', (req, res) => {
+    res.json({message: 'OK'});
+});
+
+app.listen(3000, () => {
+    console.log('Server started successfully on port');
+});
