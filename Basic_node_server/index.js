90293e2ba0139691ca3f2b0767436ddf406a0efd
naryannikhil <nikhilpareeknp@gmail.com>
2026-01-17 18:43:50 +0530

    code commit

--- Basic_node_server/index.js
+++ Basic_node_server/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
+const dotenv = require ('dotenv')
+dotenv.config();
 
 const app = express();
 
 app.get('/home', (req, res) => {
     res.json({message: 'OK'});
-});
+})
 
-app.listen(3000, () => {
-    console.log('Server started successfully on port');
-});
+app.listen(process.env.PORT, () => {
+    console.log('Server started successfully on port ${PORT}');
+})
